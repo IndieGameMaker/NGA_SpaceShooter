b63068245d3621c1f707498acbf5942c4d6fcb82
Lee JaeHyun <myleje@gmail.com>
2020-10-25 13:22:30 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -25,7 +25,12 @@ public class PlayerCtrl : MonoBehaviour
     }
 
     /* 정규화 벡터, 단위 벡터, Normalized Vector, Unit Vector (크기가 1인 벡터)
-    
+        Vector3.forward = Vector3(0, 0, 1)
+        Vector3.up      = Vector3(0, 1, 0)
+        Vector3.right   = Vector3(1, 0, 0)
+
+        Vector3.zero    = Vector3(0, 0, 0)
+        Vector3.one     = Vector3(1, 1, 1)
     */
 
  
