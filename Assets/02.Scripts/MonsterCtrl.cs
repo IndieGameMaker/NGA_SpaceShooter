49ae57084783407ac69c02c85f07ecdee6e6679d
Lee JaeHyun <myleje@gmail.com>
2020-11-01 15:24:39 +0900

    .

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    private Transform monsterTr;
+    private Transform playerTr;
+
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        monsterTr = GetComponent<Transform>();
+        playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTr.position);
+    }
+}
