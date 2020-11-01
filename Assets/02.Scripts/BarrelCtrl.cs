50063b986bdabbc6bb63d43e64f774940a663db9
Lee JaeHyun <myleje@gmail.com>
2020-11-01 14:19:17 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class BarrelCtrl : MonoBehaviour
 {
+    public GameObject expEffect;
+    private int hitCount = 0;
 
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                //드럼통 폭발효과
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        
+    }
 }
